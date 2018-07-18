9fbc52d6c40851df4951bb79a2a04f3730f37bfd
Abaz Udosen <abudosen@gmail.com>
2018-07-18 01:58:06 +0100

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://abaz:jesuschrist84990@ds141661.mlab.com:41661/vidjot_production'
+        mongoURI: 'mongodb://abazudosen:jesuschrist84990@ds141221.mlab.com:41221/vidjot'
     }
 } else {
     module.exports = {
